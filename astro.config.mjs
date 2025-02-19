32d5c99a27d75466fc6ca4926d32af7bcabd534f
jaspery <jasper.yzh@gmail.com>
2025-02-19 11:29:44 +0800

    update assets to astro:assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jasperyzh.github.io',
-  base: '/loveartdesignstudio/',
+  base: '/loveartdesignstudio',
+  asset: '/loveartdesignstudio/assets/',
 });
