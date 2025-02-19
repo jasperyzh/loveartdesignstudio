1dca1fed473ff50da2d76fdcc5897f7575cd870c
jaspery <jasper.yzh@gmail.com>
2025-02-19 11:04:31 +0800

    Fix base URL for asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jasperyzh.github.io',
-  base: '/loveartdesignstudio',
+  base: '/loveartdesignstudio/',
 });
